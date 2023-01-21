d71949225a4a92c9ff3ed17cc65d2a0e84b7118c
bfren <bf@bfren.dev>
2023-01-21 13:50:46 +0000

    v1.3.9 (#23)
    Minor updates
    * Using DEBIAN_FRONTEND=noninteractive and TERM=dumb for install
    * Don't use clear in bf-tz
    
    Build updates
    * Using fail fast false
    * Adding container description
    * Don't publish to ghcr.io
    * Removing sid

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -7,7 +7,7 @@ docker pull bfren/alpine
 BUSYBOX_VERSION="1.35.0"
 echo "Busybox: ${BUSYBOX_VERSION}"
 
-DEBIAN_VERSIONS="10 11 12 sid"
+DEBIAN_VERSIONS="10 11 12"
 for V in ${DEBIAN_VERSIONS} ; do
 
     echo "Debian ${V}"
