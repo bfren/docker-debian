64e9748fd8644d2a6ecbcc1ac61a1d4681a956f0
bfren <bf@bfren.dev>
2023-06-13 10:00:58 +0100

    Handling 12.0 / 12 busybox image label

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -13,12 +13,18 @@ for V in ${DEBIAN_VERSIONS} ; do
     echo "Debian ${V}"
     DEBIAN_MINOR=`cat ./${V}/DEBIAN_MINOR`
 
+    if [ "${DEBIAN_MINOR}" = "12.0" ] ; then
+        BUSYBOX_IMAGE="${BUSYBOX_VERSION}-debian12"
+    else
+        BUSYBOX_IMAGE="${BUSYBOX_VERSION}-debian${DEBIAN_MINOR}"
+    fi
+
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
         -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
-        BUSYBOX_VERSION=${BUSYBOX_VERSION} \
+        BUSYBOX_IMAGE=${BUSYBOX_IMAGE} \
         DEBIAN_VERSION=${V} \
         DEBIAN_MINOR=${DEBIAN_MINOR}
     )
