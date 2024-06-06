64dcb824e33ff57c41a5b5d9df9af6f829774981
bfren <bf@bfren.dev>
2024-06-06 14:03:43 +0100

    Using latest Alpine branch Updating to Nushell 0.94.2

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-ALPINE_BRANCH="v2.3.0"
+ALPINE_BRANCH="v2.3.1"
 BUSYBOX_VERSION="1.36.1"
-NUSHELL_VERSION="0.93.0"
+NUSHELL_VERSION="0.94.2"
 DEBIAN_VERSIONS="11 12 13"
 
 for V in ${DEBIAN_VERSIONS} ; do
